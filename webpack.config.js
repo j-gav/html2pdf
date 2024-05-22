c7e829203c8f41bf713d53a9fb8e85698583e56a
John Gavin <jgavin2@opentext.com>
2024-05-22 15:04:18 +0100

    bump jspdf, webpack babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 /* global __dirname, require, module*/
 
-const webpack = require('webpack');
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 const env = require('yargs').argv.env; // use --env with webpack 2
 // const pkg = require('./package.json');
 
@@ -11,33 +10,36 @@ let libraryName = 'html2pdf';
 let plugins = [], outputFile;
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  mode = 'production';
   outputFile = libraryName + '.min.js';
 } else {
+  mode = 'development';
   outputFile = libraryName + '.js';
 }
 
 const config = {
   entry: __dirname + '/src/index.js',
+  mode,
   devtool: 'source-map',
   output: {
-    path: __dirname + '/lib',
+    path: path.resolve(__dirname, '/lib'),
     filename: outputFile,
     library: libraryName,
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
+    globalObject: 'this'
   },
   module: {
     rules: [
       {
         test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
         exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
       }
     ]
   },
@@ -45,7 +47,17 @@ const config = {
     modules: [path.resolve('./node_modules'), path.resolve('./src')],
     extensions: ['.json', '.js']
   },
-  plugins: plugins
+  optimization: {
+    minimize: env === 'build',
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        output: {
+          comments: false
+        }
+      },
+      extractComments: false
+    })]
+  }
 };
 
 module.exports = config;
